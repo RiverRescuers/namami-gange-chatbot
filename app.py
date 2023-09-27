c0a658b7b190f0b8b2898f38586b808e6d22f92f
Vy <vyom.j@protonmail.com>
2023-09-28 02:19:28 +0530

    add test-chat endpoint

--- app.py
+++ app.py
@@ -4,24 +4,39 @@ from model import get_response
 
 app = Flask(__name__)
 CORS(app)
+
+
 @app.route("/", methods=["GET"])
 def index():
     return "Namami Gange RetrievalQA chain"
 
+
 @app.route("/chat", methods=["POST"])
 def chat():
-    query = request.json['query']
-    
-    if (len(query) > 500):
-        response = jsonify({'query': query, 'response': "ERROR: query too long"})
+    query = request.json["query"]
+
+    if len(query) > 500:
+        response = jsonify({"query": query, "response": "ERROR: query too long"})
         status_code = 404
     else:
-        response = jsonify({
-        'query': query,
-        'response': get_response(query)
-    })
+        response = jsonify({"query": query, "response": get_response(query)})
         status_code = 200
     return response, status_code
 
-if __name__ == '__main__':
+
+@app.route("/test-chat", methods=["GET"])
+def test_chat():
+    args = request.args
+    query = args.get("query")
+
+    if len(query) > 500:
+        response = jsonify({"query": query, "response": "ERROR: query too long"})
+        status_code = 404
+    else:
+        response = jsonify({"query": query, "response": get_response(query)})
+        status_code = 200
+    return response, status_code
+
+
+if __name__ == "__main__":
     app.run()
